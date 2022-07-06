8656bcf7daae4003ebf1f4638f39f84d9f57328e
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-07-06 03:11:02 -0300

    ensure vehicle favorite

--- src/modules/vehicle/services/tests/no-favorite-vehicle-service.spec.ts
+++ src/modules/vehicle/services/tests/no-favorite-vehicle-service.spec.ts
@@ -20,4 +20,22 @@ describe('Disfavor Vehicle', () => {
       }),
     ).rejects.toBeInstanceOf(Error);
   });
+
+  it('shold be ablet to no favorite a vehicle', async () => {
+    const vehicle = await vehicleRepositoryInMemory.create({
+      user_id: '2f860026-95cf-4942-8f01-9af157986a90',
+      name: 'First Vehicle',
+      description: 'This is a description of first vehicle',
+      plate: 'DDT-0007',
+      year: 2020,
+      color: '#ff00ff',
+      price: 22000,
+    });
+
+    await noFavoriteVehicleService.execute({
+      id: vehicle.id,
+    });
+
+    expect(vehicle.isFavorite).toBe(false);
+  });
 });
