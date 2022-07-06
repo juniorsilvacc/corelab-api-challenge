3ff938a25e52cc054087840f538e984c7b74ff6b
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-07-06 03:37:36 -0300

    ensure not to update with the same license plate

--- src/modules/vehicle/services/tests/update-vehicle-service.spec.ts
+++ src/modules/vehicle/services/tests/update-vehicle-service.spec.ts
@@ -24,4 +24,28 @@ describe('Update Vehicle', () => {
       }),
     ).rejects.toBeInstanceOf(Error);
   });
+
+  it('should not be able to change to another vehicle plate', async () => {
+    await vehicleRepositoryInMemory.create({
+      user_id: '9af157986a90-4942-4942-9af157986a90',
+      name: 'First Vehicle 2',
+      description: 'This is a description of first vehicle',
+      plate: 'DDT-0007',
+      year: 2020,
+      color: '#ff00ff',
+      price: 22000,
+    });
+
+    await expect(
+      updateVehicleService.execute({
+        id: '9af157986a90-4942-4942-9af157986a90',
+        name: 'First Vehicle 2',
+        description: 'This is a description of first vehicle',
+        plate: 'DDT-0007',
+        year: 2020,
+        color: '#ff00ff',
+        price: 22000,
+      }),
+    ).rejects.toBeInstanceOf(Error);
+  });
 });
