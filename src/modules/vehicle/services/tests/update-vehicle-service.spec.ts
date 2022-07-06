b672f6032cae4e258bc4325f532f7da07ba7444c
juniorsilvacc <juniorsilvacc@hotmail.com>
2022-07-06 03:54:12 -0300

    ensure upgrade vehicle

--- src/modules/vehicle/services/tests/update-vehicle-service.spec.ts
+++ src/modules/vehicle/services/tests/update-vehicle-service.spec.ts
@@ -48,4 +48,33 @@ describe('Update Vehicle', () => {
       }),
     ).rejects.toBeInstanceOf(Error);
   });
+
+  it('should be able to update', async () => {
+    const vehicle = await vehicleRepositoryInMemory.create({
+      user_id: '9af157986a90-4942-4942-9af157986a90',
+      name: 'First Vehicle',
+      description: 'This is a description of first vehicle',
+      plate: 'DDT-0007',
+      year: 2020,
+      color: '#ff00ff',
+      price: 22000,
+    });
+
+    await updateVehicleService.execute({
+      id: vehicle.id,
+      name: 'First Vehicle 2',
+      description: 'This is a description of first vehicle',
+      plate: 'DDT-0007',
+      year: 2020,
+      color: '#ff00ff',
+      price: 22000,
+    });
+
+    expect(vehicle.name).toBe('First Vehicle 2');
+    expect(vehicle.description).toBe('This is a description of first vehicle');
+    expect(vehicle.plate).toBe('DDT-0007');
+    expect(vehicle.year).toBe(2020);
+    expect(vehicle.color).toBe('#ff00ff');
+    expect(vehicle.price).toBe(22000);
+  });
 });
